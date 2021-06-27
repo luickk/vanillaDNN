e49065a4faf9d7988969aff2b27dcf8e900a4d49
Luick Klippel <luickklippel@MacBook.local>
2021-06-27 23:22:17 +0200

    adding dataset parsing & nPredict

--- dnn.cpp
+++ dnn.cpp
@@ -2,33 +2,94 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef void (*activationFunc)(double inp, double out);
+typedef void (*activationFunc)(float inp, float out);
+
+enum layerType {
+    fullyConnected,
+};
 
 typedef struct {
-  double *bias;
-  double *weights;
+  float *bias;
+  float *weights;
   activationFunc actFunc;
+  int size;
+  layerType type;
 } baseLayer;
+
+/*
+util functions
+*/
+int trainDNN(char pathToFile[]) {
+  FILE *fp;
+  char *line = 0;
+  float inp[4] = {0};
+  int inpIt = 0;
+  size_t len = 0;
+  ssize_t read;
+
+  fp = fopen(pathToFile, "r");
+  if (fp == 0) {
+   return 1;
+  }
+  while ((read = getline(&line, &len, fp)) != -1) {
+    if (inpIt == 4) {
+      printf("\n ");
+      for(int j = 0; j <4; j++) {
+        printf("%f ", inp[j]);
+      }
+      inpIt = 0;
+    }
+    inp[inpIt] = atof(line);
+    inpIt++;
+  }
+  fclose(fp);
+  if (line) {
+   free(line);
+  }
+  return 0;
+}
 
 /*
 general NN functions
 */
-void initLayer(double size, baseLayer &layer, activationFunc actFunc) {
-  layer.bias = (double*)malloc(size * sizeof(double));
-  layer.weights = (double*)malloc(size * sizeof(double));
+void initLayer(float size, layerType type, baseLayer &layer, activationFunc actFunc) {
+  layer.bias = (float*)malloc(size * sizeof(float));
+  layer.weights = (float*)malloc(size * sizeof(float));
   layer.actFunc = actFunc;
+  layer.size = size;
+  layer.type = type;
+}
+
+baseLayer *createLayer(float size, layerType type, activationFunc actFunc) {
+  static baseLayer layer;
+  initLayer(size, fullyConnected, layer, actFunc);
+  return &layer;
 }
 
 /*
 Layer Operations
 */
-
-void testActFunc(double inp, double out){
-
+void testActFunc(float inp, float out){
+  if (inp < 1) {
+    out = 0;
+  } else if (inp >=1) {
+    out = 1;
+  }
 }
 
 int main(){
-  printf("test \n");
-  baseLayer hiddenLayer1;
-  initLayer(100, hiddenLayer1, testActFunc);
+  int nPredict = 5;
+
+  printf("nPredict: %d \n", nPredict);
+
+  baseLayer *inpLayer = createLayer(nPredict, fullyConnected, testActFunc);
+  baseLayer *hiddenLayer2 = createLayer(100, fullyConnected, testActFunc);
+  baseLayer *hiddenLayer1 = createLayer(100, fullyConnected, testActFunc);
+  baseLayer *outpLayer = createLayer(nPredict, fullyConnected, testActFunc);
+
+  int rc = trainDNN("../data/datasetByLine.csv");
+  if (rc != 0) {
+     printf("file open failed");
+     return 0;
+  }
 }
